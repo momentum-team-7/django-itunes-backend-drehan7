480d1a2146c27ce412a9d1d58d7be820475b07f5
Dawud Rehan <d.rehan7@gmail.com>
2021-02-28 20:20:51 -0500

    tried to figure out modeling; didnt pan out

--- core/models.py
+++ core/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     pass
+
+
+class Recent(models.Model):
+    song_name = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.artist} | {self.song_name}"
--- core/views.py
+++ core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Recent
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'core/index.html', {})
+
+
+def recent(request):
+    recent_songs = Recent.objects.all()
+    return render(request, 'core/recents.html', {'recent_songs': recent_songs})
